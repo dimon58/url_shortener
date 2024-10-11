ed7f627c0931a1965639ee355db923a6a773efaa
Dmitry <dimasharonov95@yandex.ru>
2024-10-11 16:36:54 +0300

    Добавлен парсинг user-agent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging.config
 
+import user_agents
 import uvicorn
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import RedirectResponse, PlainTextResponse
@@ -28,13 +29,26 @@ async def generate_short_url(url: str, request: Request) -> PlainTextResponse:
 async def redirect(path: str, request: Request):
     async with async_session_maker() as db_session, db_session.begin():
         short_url = await ShortUrl.get_full_url(db_session, path)
-
+        ua = user_agents.parse(request.headers["User-Agent"])
         click = Click(
             short_path=path,
             query_params=dict(request.query_params),
             headers=dict(request.headers),
             cookies=request.cookies,
-            success=short_url is not None
+            success=short_url is not None,
+            user_agent_device_family=ua.device.family,
+            user_agent_device_brand=ua.device.brand,
+            user_agent_device_model=ua.device.model,
+            user_agent_os_family=ua.os.family,
+            user_agent_os_version=ua.os.version_string,
+            user_agent_browser_family=ua.browser.family,
+            user_agent_browser_version=ua.browser.version_string,
+            user_agent_is_pc=ua.is_pc,
+            user_agent_is_mobile=ua.is_mobile,
+            user_agent_is_tablet=ua.is_tablet,
+            user_agent_is_touch_capable=ua.is_touch_capable,
+            user_agent_is_bot=ua.is_bot,
+            user_agent_is_email_client=ua.is_email_client,
         )
         db_session.add(click)
 
@@ -44,5 +58,5 @@ async def redirect(path: str, request: Request):
     return RedirectResponse(short_url.full_url, status_code=307)
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8090, log_level="info")
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8090, log_level="info")
--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import logging
 
-from sqlalchemy import Column, select, exists
+from sqlalchemy import select, exists
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import sqltypes
 
 from configs import URL_LENGTH
@@ -13,17 +13,19 @@ logger = logging.getLogger(__name__)
 
 
 class ShortUrl(BaseModel):
-    short_path: Mapped[str] = Column(sqltypes.VARCHAR(8), nullable=False, index=True)
-    full_url: Mapped[str] = Column(sqltypes.String, nullable=False)
-    meta: Mapped[dict] = Column(sqltypes.JSON, default=dict)
+    short_path: Mapped[str] = mapped_column(sqltypes.VARCHAR(8), index=True)
+    full_url: Mapped[str] = mapped_column(sqltypes.String)
+    meta: Mapped[dict] = mapped_column(sqltypes.JSON, default=dict)
 
     @classmethod
     async def get_full_url(cls, db_session: AsyncSession, short_path: str):
+        # noinspection PyTypeChecker
         stmt = select(cls).where(cls.short_path == short_path)
         return await db_session.scalar(stmt)
 
     @classmethod
     async def exists(cls, db_session: AsyncSession, short_path: str) -> bool:
+        # noinspection PyTestUnpassedFixture
         stmt = exists().select_from(cls).where(cls.short_path == short_path).select()
         return await db_session.scalar(stmt)
 
@@ -45,9 +47,62 @@ class ShortUrl(BaseModel):
 
 
 class Click(BaseModel):
-    short_path: Mapped[str] = Column(sqltypes.VARCHAR(8), nullable=False)
+    short_path: Mapped[str] = mapped_column(sqltypes.VARCHAR(8))
 
-    query_params: Mapped[dict] = Column(sqltypes.JSON, default=dict)
-    headers: Mapped[dict] = Column(sqltypes.JSON, default=dict)
-    cookies: Mapped[dict] = Column(sqltypes.JSON, default=dict)
-    success: Mapped[bool] = Column(sqltypes.Boolean)
+    query_params: Mapped[dict] = mapped_column(sqltypes.JSON, default=dict)
+    headers: Mapped[dict] = mapped_column(sqltypes.JSON, default=dict)
+    cookies: Mapped[dict] = mapped_column(sqltypes.JSON, default=dict)
+    success: Mapped[bool] = mapped_column(sqltypes.Boolean)
+
+    # Распаршенный User-Agent
+    # Устройство
+    user_agent_device_family: Mapped[str] = mapped_column(
+        doc="Device family", default=""
+    )
+    user_agent_device_brand: Mapped[str] = mapped_column(doc="Device brand", default="")
+    user_agent_device_model: Mapped[str] = mapped_column(doc="Device model", default="")
+
+    # Операционная система
+    user_agent_os_family: Mapped[str] = mapped_column(
+        doc="Operating system family", default=""
+    )
+    user_agent_os_version: Mapped[str] = mapped_column(
+        doc="Operating system version", default=""
+    )
+
+    # Браузер
+    user_agent_browser_family: Mapped[str] = mapped_column(
+        doc="Browser family", default=""
+    )
+    user_agent_browser_version: Mapped[str] = mapped_column(
+        doc="Browser version", default=""
+    )
+
+    # Кем является клиент
+    user_agent_is_pc: Mapped[bool] = mapped_column(
+        doc='whether user agent is identified to be running a traditional "desktop" OS (Windows, OS X, Linux)',
+        default=False,
+    )
+    user_agent_is_mobile: Mapped[bool] = mapped_column(
+        doc=(
+            "whether user agent is identified as a mobile phone "
+            "(iPhone, Android phones, Blackberry, Windows Phone devices etc)"
+        ),
+        default=False,
+    )
+    user_agent_is_tablet: Mapped[bool] = mapped_column(
+        doc="whether user agent is identified as a tablet device (iPad, Kindle Fire, Nexus 7 etc)",
+        default=False,
+    )
+    user_agent_is_touch_capable: Mapped[bool] = mapped_column(
+        doc="whether user agent has touch capabilities",
+        default=False,
+    )
+    user_agent_is_bot: Mapped[bool] = mapped_column(
+        doc="whether user agent is a search engine crawler/spider",
+        default=False,
+    )
+    user_agent_is_email_client: Mapped[bool] = mapped_column(
+        doc="whether user agent is a email client",
+        default=False,
+    )
